6434f82a6c481d976b2b1aa874f2de64dceaee6b
JoeEverest <everestjoseph11@yahoo.com>
2024-11-26 15:16:29 -0600

    update Q4 logic and variable names

--- src/lesson2/Prog4.java
+++ src/lesson2/Prog4.java
@@ -5,24 +5,26 @@ import java.util.Arrays;
 public class Prog4 {
     public static void main(String[] args) {
         String[] originalArray = {"horse", "dog", "cat", "horse", "dog"};
+        int orgLen = originalArray.length;
 
-        int originalCount = 1;
-        for (int i = 0; i < originalArray.length; i++) {
+        int uniqueWords = originalArray.length;
+
+        for (int i = 0; i < orgLen; i++) {
             String currentWord = originalArray[i];
 
-            for (int j = i+1; j < originalArray.length; j++) {
+            for (int j = i+1; j < orgLen; j++) {
                 if(currentWord == null) break;
                 if (currentWord.equals(originalArray[j])){
-                    ++originalCount;
+                    uniqueWords--;
                     originalArray[j] = null;
                 }
             }
         }
 
-        String[] arrayWithoutDuplicates = new String[originalCount];
+        String[] arrayWithoutDuplicates = new String[uniqueWords];
 
-        for (int i = 0; i < originalCount; i++) {
-            for (int j = 0; j < originalArray.length; j++) {
+        for (int i = 0; i < uniqueWords; i++) {
+            for (int j = 0; j < orgLen; j++) {
                 if(originalArray[j] != null){
                     arrayWithoutDuplicates[i] = originalArray[j];
                     originalArray[j] = null;
@@ -32,6 +34,5 @@ public class Prog4 {
         }
 
         System.out.println(Arrays.toString(arrayWithoutDuplicates));
-
     }
 }
